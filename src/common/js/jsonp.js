10d1b2a659d84bffe8071570751355537376a132
stepbystep999 <stepbystep6037@163.com>
2017-10-31 00:30:23 +0800

    代码提交

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,25 @@
+import originJSONP from 'jsonp'
+
+export default function jsonp(url, data, option) {
+    url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+    return new Promise((resolve, reject) => {
+        originJSONP(url, option, (err, data) => {
+            if (!err) {
+                resolve(data)
+            }
+            else {
+                reject(err)
+            }
+        })
+    })
+}
+
+function param(data) {
+    let url = ''
+    for (var k in data) {
+        let value = data[k] !== undefined ? data[k] : ''
+        url += `&${k}=${encodeURIComponent(value)}` //encodeURIComponent是个JS方法，可以转换一些特殊的符号
+    }
+    return url ? url.substring(1) : ''  //去掉第一个&符号，以避免和回调函数中的&重复
+}
