093271651c680b9a5cf449df7894d6cb2579f521
stepbystep999 <stepbystep6037@163.com>
2017-10-31 00:33:40 +0800

    代码提交

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -7,8 +7,7 @@ export default function jsonp(url, data, option) {
         originJSONP(url, option, (err, data) => {
             if (!err) {
                 resolve(data)
-            }
-            else {
+            } else {
                 reject(err)
             }
         })
@@ -19,7 +18,7 @@ function param(data) {
     let url = ''
     for (var k in data) {
         let value = data[k] !== undefined ? data[k] : ''
-        url += `&${k}=${encodeURIComponent(value)}` //encodeURIComponent是个JS方法，可以转换一些特殊的符号
+        url += `&${k}=${encodeURIComponent(value)}` // encodeURIComponent是个JS方法，可以转换一些特殊的符号
     }
-    return url ? url.substring(1) : ''  //去掉第一个&符号，以避免和回调函数中的&重复
+    return url ? url.substring(1) : ''  // 去掉第一个&符号，以避免和回调函数中的&重复
 }
